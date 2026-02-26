a723bfba24ab36e7eb21c5be8a6984edd00e0f49
Talha Munawar <64393511+talhamsajid@users.noreply.github.com>
2026-02-26 10:13:51 +0500

    axon-pro v0.3.4

--- Formula/axon-pro.rb
+++ Formula/axon-pro.rb
@@ -3,8 +3,8 @@ class AxonPro < Formula
 
   desc "Enterprise-grade graph-powered code intelligence engine"
   homepage "https://github.com/talhamsajid/axon-pro"
-  url "https://github.com/talhamsajid/axon-pro/archive/refs/tags/v0.3.3.tar.gz"
-  sha256 "dcedd2649cef75f459c35f37b57a65e15863de6965987cd6619350708ad50e38"
+  url "https://github.com/talhamsajid/axon-pro/archive/refs/tags/v0.3.4.tar.gz"
+  sha256 "91e33ffdd0168be81b64d1e87c9ecfe0f00f37f6d371b091e94c4a60cbc99d4a"
   license "MIT"
 
   depends_on "python@3.12"
